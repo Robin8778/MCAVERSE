6044da2c88fd4dd1716340d601cbea9cd6592d2d
RobinSingh08 <rsingh_mca21@thapar.edu>
2023-03-10 23:47:09 +0530

    first commit

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/skywalk.png',
+    title: 'SkyWalk',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/HostelA.jpg',
+    title: 'Hostel A',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/PGHostel.JPG',
+    title: 'PG Hostel',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/COS.JPG',
+    title: 'COS',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/CoffeeThapar.jpg',
+    title: 'Admin',
+  },
+];
+
+export const startingFeatures = [
+  'Select the place in thapar you want to enter',
+  'Enter the place to see pictures',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'To get and have a meeting in virtual environment in Thapar university',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making Thapar more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
---
+++ pages/photos.js
@@ -0,0 +1,107 @@
+'use client';
+import '../styles/globals.css';
+import { Footer, Navbar } from '../components';
+import styles from '../styles';
+
+const Photos = () => {
+    return (
+        <>
+            <title>MCA❤️VERSE</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/favicon.ico" />
+            <div className="bg-primary-black overflow-hidden">
+                <Navbar />
+
+                <section class="overflow-hidden text-neutral-700">
+                    <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+                        <div class="-m-1 flex flex-wrap md:-m-2">
+                            <div class="flex w-1/2 flex-wrap">
+                                <div class="w-1/2 p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/1.png" />
+                                </div>
+                                <div class="w-1/2 p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/2.png" />
+                                </div>
+                                <div class="w-full p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/12.JPG" />
+                                </div>
+                            </div>
+                            <div class="flex w-1/2 flex-wrap">
+                                <div class="w-full p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/4.png" />
+                                </div>
+                                <div class="w-1/2 p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/5.png" />
+                                </div>
+                                <div class="w-1/2 p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/6.png" />
+                                </div>
+                            </div>
+                            <div class="flex w-1/2 flex-wrap">
+                                <div class="w-full p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/7.png" />
+                                </div>
+                                <div class="w-1/2 p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/8.png" />
+                                </div>
+                                <div class="w-1/2 p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/9.png" />
+                                </div>
+                            </div>
+                            <div class="flex w-1/2 flex-wrap">
+                                <div class="w-full p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/10.png" />
+                                </div>
+                                <div class="w-1/2 p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/11.png" />
+                                </div>
+                                <div class="w-1/2 p-1 md:p-2">
+                                    <img
+                                        alt="gallery"
+                                        class="block h-full w-full rounded-lg object-cover object-center"
+                                        src="/photos/12.JPG" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <Footer />
+            </div>
+        </>
+    )
+}
+
+export default Photos;
